3beaf9cea2d365f8b35cb7bed3ca98260bee63f8
xue1992115 <1356351760@qq.com>
2023-02-07 14:36:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-v4vmcb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-v4vmcb)
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import MonacoEditor from 'monaco-editor-vue3';
+
+export default {
+  components: {
+    MonacoEditor,
+  },
+  data() {
+    return {
+      options: {
+        colorDecorators: true,
+        lineHeight: 24,
+        tabSize: 2,
+      },
+      test: '',
+    };
+  },
+};
+</script>
+
+<template>
+  <h3>Monaco Editor Vue3</h3>
+  <MonacoEditor
+    theme="vs"
+    :options="options"
+    language="javascript"
+    :width="800"
+    :height="600"
+    v-model:value="test"
+  ></MonacoEditor>
+</template>
+
+<style scoped></style>
